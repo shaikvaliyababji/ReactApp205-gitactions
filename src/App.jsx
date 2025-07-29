1a6cea2ee9d1b3240200e3140f7cc582ec39c3c0
babji <2300039117@kluniversity.in>
2025-07-29 12:37:28 +0530

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ function App() {
     <div className="container">
       <h1>Welcome to My React Project - S205</h1>
       <p>CI/CD  cloud DevOps</p>
-      <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
+      <p>This is a simple homepage deployed on GitHub Actions using Vite.</p>
+      <p>done with git pages and git Actions</p>
 
     </div>
   );
